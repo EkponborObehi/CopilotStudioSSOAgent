9740459cda494d6e5239c3660cb1f722df45a32d
EkponborObehi <ekponborobehi@gmail.com>
2025-11-14 13:50:49 +0100

    update on config

---
+++ assets/js/config.js
@@ -0,0 +1,7 @@
+// Configuration file for SSO Agent
+const CONFIG = {
+  clientId: window.ENV?.CLIENTID,
+  tenantId: window.ENV?.TENANTID,
+  tokenEndpoint: window.ENV?.TOKENENDPOINT,
+  customScope: window.ENV?.CUSTOMSCOPE,
+};
