75a499efbc22adbba75d5e10e1ca0cab54b3e328
EkponborObehi <ekponborobehi@gmail.com>
2025-11-14 17:03:25 +0100

    env fix

--- assets/js/env.js
+++
@@ -1,6 +0,0 @@
-window.ENV = {
-  CLIENTID:"a1e38d29-55f5-4110-af53-dfb1aa56c6e1",
-  TENANTID:"a04a5b96-5d86-4d00-b35b-bc7e2a22f7dd",
-  TOKENENDPOINT:"https://fcced93a5eb5e69b8c70fd6a9cb6ca.46.environment.api.powerplatform.com/powervirtualagents/botsbyschema/cre0e_agent/directline/token?api-version=2022-03-01-preview",
-  CUSTOMSCOPE:"api://bd7f746d-48d6-4a75-a59a-02e1a3b7d90b/access_as_user"
-};
